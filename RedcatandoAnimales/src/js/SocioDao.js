bb42605afd388b21c0ac5f631141e1197f224039
MarceloHidalgo <marcelohidalgo624@gmail.com>
2019-05-08 15:10:48 -0400

    modificado SocioDao.js

--- RedcatandoAnimales/src/js/SocioDao.js
+++ RedcatandoAnimales/src/js/SocioDao.js
@@ -9,18 +9,18 @@ var con = mysql.createConnection({
 });
 
 function VerificarSocio(rut, contraseña, asociacion, rol) {
-    // Generar nueva consulta con estos cuatro parametros
+    
     //coneccion a bd
     con.connect(function(err) {
         if (err) throw err;
         console.log("Connected!");
 
         //Query
-        var sql = "SELECT COUNT(COD) FROM socio WHERE NOMBRE = '"+nombre+"' AND CONTRASENA = '"+contraseña+"'";
+        var sql = "SELECT COUNT(COD) FROM socio WHERE COD = '"+rut+"'";
         con.query(sql, function (err, result) {
             if (err) throw err;
             console.log(result);
         });
     });
-    return true;
+    return result;
 }
