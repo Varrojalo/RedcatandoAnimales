6f3c2bc55ec9f22ef3718c818abfc1c5320dd737
Alonso Rojas <contact.alonsorojas@gmail.com>
2019-06-10 23:26:49 -0400

    Arreglados nombres de funciones de DireccionDao.php

--- RedcatandoAnimales/src/php/DireccionDao.php
+++ RedcatandoAnimales/src/php/DireccionDao.php
@@ -2,48 +2,49 @@
 include 'Conexion.php';
 include 'model/dto/Direccion.php';
 
-function buscarDireccion(){
-    // Crea conexion
-    $con = new Conexion();
-    $conn = $con->Conectar();
-    $sql = $conn->prepare("SELECT * FROM direccion");
-    $result = $conn->query($sql);
-
-    $tabla = array();
-
-    if ($result->num_rows > 0) {
-        // almacena resultado en arreglo
-        while($fila = $result->fetch_assoc()) {
-            $tabla[] = new Direccion($fila["cod"], $fila["codDuenio"], $fila["numero"],
-            $fila["calle"], $fila["descripcion"]);
+class DireccionDao
+{
+    function buscarDireccion(){
+        // Crea conexion
+        $con = new Conexion();
+        $conn = $con->Conectar();
+        $sql = $conn->prepare("SELECT * FROM direccion");
+        $result = $conn->query($sql);
+    
+        $tabla = array();
+    
+        if ($result->num_rows > 0) {
+            // almacena resultado en arreglo
+            while($fila = $result->fetch_assoc()) {
+                $tabla[] = new Direccion($fila["cod"], $fila["codDuenio"], $fila["numero"],
+                $fila["calle"], $fila["descripcion"]);
+            }
+            return $tabla;
+        } else {
+            echo "0 results";
+            return null;
         }
-        return $tabla;
-    } else {
-        echo "0 results";
-        return null;
+        $con->Desconectar();
     }
-    $con->Desconectar();
+    
+    function agregarDireccion($campaña)
+    {
+        $con = new Conexion();
+        $conn = $con->Conectar();
+        $sql = $conn->prepare("INSERT INTO direccion (cod, codDuenio, numero, calle, descripcion) VALUES (?,?,?,?,?)");
+        $sql = $conn->bind_param($campaña->getCodigo(), $campaña->getDuenio(), $campaña->getNumero(), $campaña->getCalle(), $campaña->getDescripcion(), $campaña->getTipo());
+        $result = $conn->query($sql);
+    }
+    
+    function eliminarDireccion($codigo)
+    {
+        $con = new Conexion();
+        $conn = $con->Conectar();
+        $sql = "DELETE * FROM direccion WHERE cod = ?";
+        $statement =$conn->prepare($sql);
+        $statement->bind_param($codigo);
+        $statement->execute();
+        $con->Desconectar();
+    }
 }
-
-function agregarCampaña($campaña)
-{
-    $con = new Conexion();
-    $conn = $con->Conectar();
-    $sql = $conn->prepare("INSERT INTO direccion (cod, codDuenio, numero, calle, descripcion)
-    VALUES (?,?,?,?,?)");
-    $sql = $conn->bind_param($campaña->getCodigo(), $campaña->getDuenio(), $campaña->getNumero(), $campaña->getCalle(), $campaña->getDescripcion(), $campaña->getTipo());
-    $result = $conn->query($sql);
-}
-
-function eliminarCampaña($codigo)
-{
-    $con = new Conexion();
-    $conn = $con->Conectar();
-    $sql = $conn->prepare("DELETE * FROM direccion WHERE cod = ?");
-    $sql->bind_param($codigo)
-    $result = $conn->query($sql);
-
-    $con->Desconectar();
-}
-
 ?>
