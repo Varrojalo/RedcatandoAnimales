bd39646944a6b836d903d799961e5d582a1fb309
Alonso Rojas <contact.alonsorojas@gmail.com>
2019-06-10 22:42:29 -0400

    Completado AnimalDao

--- RedcatandoAnimales/src/php/AnimalDao.php
+++ RedcatandoAnimales/src/php/AnimalDao.php
@@ -2,39 +2,51 @@
 include 'Conexion.php';
 include 'model/dto/Animal.php';
 
-function buscarAnimales(){
-    // Crea conexion
-    $con = new Conexion();
-    $conn = $con->Conectar();
-    $sql = "SELECT * FROM animal";
-    $result = $conn->query($sql);
-
-    $tabla = array();
-
-    if ($result->num_rows > 0) {
-        // almacena resultado en arreglo
-        while($fila = $result->fetch_assoc()) {
-            $tabla[] = new Animal($fila["cod"], $fila["codOrganizacion"], $fila["codDueno"],
-            $fila["nombre"], $fila["edad"], $fila["fechaIngreso"], $fila["especie"], $fila["raza"], 
-            $fila["patron"], $fila["sexo"], $fila["observacion"], $fila["chip"]);
-        }
-        return $tabla;
-    } else {
-        echo "0 results";
-        return null;
-    }
-    $con->Desconectar();
-}
-
-function eliminarAnimal(var $codigo)
+class AnimalDao
 {
-    $con = new Conexion();
-    $conn = $con->Conectar();
-    $sql = "DELETE * FROM animal WHERE cod = ".$codigo;
-    $result = $conn->query($sql);
-
-    $con->Desconectar();
+    function buscarAnimales(){
+        // Crea conexion
+        $con = new Conexion();
+        $conn = $con->Conectar();
+        $sql = "SELECT * FROM animal";
+        $result = $conn->query($sql);
+    
+        $lista = array();
+    
+        if ($result->num_rows > 0) {
+            // almacena resultado en arreglo
+            while($fila = $result->fetch_assoc()) {
+                $lista[] = new Animal($fila["cod"], $fila["codOrganizacion"], $fila["codDueno"],
+                $fila["nombre"], $fila["edad"], $fila["fechaIngreso"], $fila["especie"], $fila["raza"], 
+                $fila["patron"], $fila["sexo"], $fila["observacion"], $fila["chip"]);
+            }
+            return $lista;
+        } else {
+            echo "0 results";
+            return null;
+        }
+        $con->Desconectar();
+    }
+    function eliminarAnimal($animal)
+    {
+        $con = new Conexion();
+        $conn = $con->Conectar();
+        $sql = "DELETE * FROM animal WHERE cod = ?";
+        $statement = $conn->prepare($sql);
+        $statement->bind_param($animal->getCodigo());
+        $statement->execute();
+        $con->Desconectar();
+    }
+    function agregarAnimal($animal)
+    {
+        $con = new Conexion();
+        $conn = $con->Conectar();
+        $sql = "INSERT INTO animal (cod, codDueno, codOrganizacion, nombre, edad, fechaIngreso, especie, raza, patron, sexo, observacion, chip) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+        $statement = $conn->prepare($sql);
+        $statement->bind_param($animal->getCodigo(),$animal->getCodigoDueño(),$animal->getCodigoOrganizacion(),$animal->getNombre(),$animal->getEdad(),$animal->getFechaIngreso(),$animal->getEspecie(),$animal->getRaza(),$animal->getPatron(),$animal->getSexo(),$animal->getObservacion(),$animal->getChip());
+        $statement->execute();
+        $con->Desconectar();
+    }
 }
-
 ?>
 
