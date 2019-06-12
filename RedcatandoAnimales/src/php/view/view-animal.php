272c1341e9944b0046059b8c6170430db926da8a
Alonso Rojas <contact.alonsorojas@gmail.com>
2019-06-12 15:01:11 -0400

    Modificado Vista Animal (Prototipo NO FUNCIONAL)

--- RedcatandoAnimales/src/php/view/view-animal.php
+++ RedcatandoAnimales/src/php/view/view-animal.php
@@ -22,11 +22,46 @@
                     <a href="" class="btn btn-link text-muted font-weight-bold"><i class="fas fa-edit"></i>  EDITAR</a>
                 </div>
             </div>
+            <div class="dropdown-divider"></div>
             <?php
                 include_once '../model/dao/AnimalDao.php';
-
-                
-            
+                echo "<div class='row'>";
+                echo "<div class='col-md-4'>";
+                echo "<h4>Pepa <span class='badge badge-primary'>ADOPTADO</span></h4>";
+                echo "</div>";
+                echo "<div class='col-md-4'>";
+                echo "<p><strong>Especie: </strong><span class='fas fa-dog'></span></p>";
+                echo "</div>";
+                echo "<div class='col-md-4'>";
+                echo "<p><strong>Raza: </strong> Quiltro</p>";
+                echo "</div>";
+                echo "</div>";
+                echo "<div class='row'>";
+                echo "<div class='col-md-3'>";
+                echo "<p><strong>Edad: </strong> 4</p>";
+                echo "</div>";
+                echo "<div class='col-md-3'>";
+                echo "<p><strong>Patron: </strong> Negro</p>";
+                echo "</div>";
+                echo "<div class='col-md-2'>";
+                echo "<p><strong>Sexo: </strong><span class='fas fa-venus '></span></p>";
+                echo "</div>";
+                echo "<div class='col-md-4'>";
+                echo "<p><strong>Fecha de Ingreso: </strong>2019-05-19</p>";
+                echo "</div>";
+                echo "</div>";
+                echo "<div class='row'>";
+                echo "<div class='col-md-6'>";
+                echo "<strong>Observacion: </strong>";
+                echo "<p>Pata derecha rota</p>";
+                echo "</div>";
+                echo "<div class='col-md-6'>";
+                echo "<h5>Datos del dueño: </h5>";
+                echo "<div class='dropdown-divider'></div>";
+                echo "<p><strong>Nº CHIP: </strong> 111111111111111</p>";
+                echo "<p><strong>Dueño: </strong> Sebastian Hidalgo Toro</p>";
+                echo "</div>";
+                echo "</div>";
             ?>
             
             <a href="history-animal.php" class="btn btn-link">ATRAS</a>
