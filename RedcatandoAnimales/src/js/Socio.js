1740102ca466a93e17bef85af5684f2a9f1f9e1a
Alonso Rojas <contact.alonsorojas@gmail.com>
2019-06-18 00:13:11 -0400

    Eliminado NODE y Js no usados, encapsulados DAOs en clases

--- RedcatandoAnimales/src/js/Socio.js
+++
@@ -1,8 +0,0 @@
-class Socio {
-    constructor(rut, contraseña, asociacion, rol) {
-        this.rut = rut;
-        this.contraseña = contraseña;
-        this.asociacion = asociacion;
-        this.rol = rol;
-    }
-}
--- RedcatandoAnimales/src/php/model/dao/SocioDao.php
+++ RedcatandoAnimales/src/php/model/dao/SocioDao.php
@@ -2,17 +2,20 @@
 include 'Conexion.php';
 include 'model/dto/Socio.php';
 
-function buscarSocio($codigo){
-    // Crea conexion
-    $con = new Conexion();
-    $conn = $con->Conectar();
-    $sql = "SELECT * FROM socio WHERE cod = ".$codigo;
-    $result = $conn->query($sql);
-
-    $socio = new Socio($result[0]["cod"], $result[0]["codOrganizacion"], $result[0]["contracena"], $result[0]["nombre"], $result[0]["apellidoPaterno"], $result[0]["cargo"] );
-
-    return $socio;
-
-    $con->Desconectar();
+class SocioDao
+{
+    function buscarSocio($codigo){
+        // Crea conexion
+        $con = new Conexion();
+        $conn = $con->Conectar();
+        $sql = "SELECT * FROM socio WHERE cod = ".$codigo;
+        $result = $conn->query($sql);
+    
+        $socio = new Socio($result[0]["cod"], $result[0]["codOrganizacion"], $result[0]["contracena"], $result[0]["nombre"], $result[0]["apellidoPaterno"], $result[0]["cargo"] );
+    
+        return $socio;
+    
+        $con->Desconectar();
+    }
 }
 ?>
